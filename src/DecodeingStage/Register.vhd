b283cb21dad38d7b7c3731cc1a03c7d0a23028e3
bassantt <bassantmohamed945@yahoo.com>
2021-06-04 16:11:21 +0200

    Reg_correct

--- src/DecodeingStage/Register.vhd
+++ src/DecodeingStage/Register.vhd
@@ -3,20 +3,20 @@ USE IEEE.std_logic_1164.ALL;
 ENTITY Reg IS
   GENERIC (n : INTEGER := 16);
   PORT (
-    clk, rst, enable : IN STD_LOGIC;
+    rst, enable : IN STD_LOGIC;
     d : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
     q : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0));
 END Reg;
 
 ARCHITECTURE a_Reg OF Reg IS
 BEGIN
-  PROCESS (clk, rst)
+  PROCESS (enable, rst)
   BEGIN
     IF (enable = '0') THEN
       NULL;
     ELSIF (rst = '1') THEN
       q <= (OTHERS => '0');
-    ELSIF clk'event AND clk = '1' THEN
+    ELSE 
       q <= d;
     END IF;
   END PROCESS;
